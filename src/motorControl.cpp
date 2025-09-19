e9f829432fa564b6a94d26080034270286578506
Iván Altamirano <ialtamirano@frba.utn.edu.ar>
2025-09-19 14:40:58 -0300

    Ejemplo de clase, usart

--- src/motorControl.cpp
+++ src/motorControl.cpp
@@ -1,34 +1,89 @@
 /*
- * Copyright 2022 NXP
- * NXP confidential.
- * This software is owned or controlled by NXP and may only be used strictly
- * in accordance with the applicable license terms.  By expressly accepting
- * such terms or by downloading, installing, activating and/or otherwise using
- * the software, you are agreeing that you have read, and that you agree to
- * comply with and are bound by, such license terms.  If you do not agree to
- * be bound by the applicable license terms, then you may not retain, install,
- * activate or otherwise use the software.
+╭──╦──╮
+│IC║XC│
+╠══╬══╣
+│NI║KA│
+╰──╩──╯
  */
-
 #include <cr_section_macros.h>
-#include "LPC845.h"
-#include "Init.h"
+#include <Init.h>
+#include <LPC845.h>
+#include <DigitalInput.h>
+#include <serialCom.h>
 
-// TODO: insert other definitions and declarations here
+uint8_t respuestaOK[] = {"LED N ENCENDIDO"};
+uint8_t bytesOK = 15;
+uint8_t respuestaERROR[] = {"ERROR"};
+uint8_t bytesERROR = 5;
 
 int main(void) {
 
 	Inicializar();
-    // TODO: insert code here
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
-    }
+	serialCom comPC ( 1 , 115200 ,  25 , 24 );
+
+	gpio ledG(1,0,gpio::SALIDA,gpio::LOW);
+	gpio ledB(1,1,gpio::SALIDA,gpio::LOW);
+	gpio ledR(1,2,gpio::SALIDA,gpio::LOW);
+
+	ledG.clrPIN();
+	ledB.clrPIN();
+	ledR.clrPIN();
+
+	int16_t dato;
+
+	while(1) {
+
+		dato = comPC.Recibir();
+
+		switch ( dato )
+		{
+		case 'r':
+
+			ledG.clrPIN();
+			ledB.clrPIN();
+			ledR.setPIN();
+			respuestaOK[4] = 'R';
+			comPC.Transmitir(respuestaOK,bytesOK);
+			comPC.Transmitir(0x0D);
+			comPC.Transmitir(0x0A);
+			break;
+
+		case 'g':
+
+			ledG.setPIN();
+			ledB.clrPIN();
+			ledR.clrPIN();
+			respuestaOK[4] = 'G';
+			comPC.Transmitir(respuestaOK,bytesOK);
+			comPC.Transmitir(0x0D);
+			comPC.Transmitir(0x0A);
+			break;
+
+		case 'b':
+
+			ledG.clrPIN();
+			ledB.setPIN();
+			ledR.clrPIN();
+			respuestaOK[4] = 'B';
+			comPC.Transmitir(respuestaOK,bytesOK);
+			comPC.Transmitir(0x0D);
+			comPC.Transmitir(0x0A);
+			break;
+
+		case -1:
+			//En este caso no hago nada
+			break;
+
+		default:
+			//Llego un dato distinto de los válidos:
+			comPC.Transmitir(respuestaERROR,bytesERROR);
+			comPC.Transmitir(0x0D);
+			comPC.Transmitir(0x0A);
+			break;
+		}
+
+	}
     return 0 ;
 }
+
