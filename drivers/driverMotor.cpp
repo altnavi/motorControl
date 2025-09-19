86eb1906512205dbc1fa618383bbe05e034e9140
Iván Altamirano <ialtamirano@frba.utn.edu.ar>
2025-09-19 17:37:05 -0300

    Últimas peticiones. Ver con equipo.

---
+++ drivers/driverMotor.cpp
@@ -0,0 +1,18 @@
+/*
+ * driverMotor.cpp
+ *
+ *  Created on: 19 sep. 2025
+ *      Author: Iván
+ */
+
+#include <driverMotor.h>
+
+driverMotor::driverMotor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+driverMotor::~driverMotor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ drivers/driverMotor.h
@@ -0,0 +1,17 @@
+/*
+ * driverMotor.h
+ *
+ *  Created on: 19 sep. 2025
+ *      Author: Iván
+ */
+
+#ifndef DRIVERMOTOR_H_
+#define DRIVERMOTOR_H_
+
+class driverMotor {
+public:
+	driverMotor();
+	virtual ~driverMotor();
+};
+
+#endif /* DRIVERMOTOR_H_ */
